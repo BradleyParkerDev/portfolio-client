d5fc8bcda260344f1b5c03480e225e97ff5adf85
Bradley Parker <Bradleyscottparker@outlook.com>
2023-10-12 18:05:19 -0500

    changed line height with leading in bi-content

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -9,10 +9,10 @@ const AboutPage = () =>{
                 <div id='ap-div1-inner' className="w-[100%] max-w-[530px]  h-[auto] mr-[20px] ml-[20px]  border-black border-dashed border-[1px]">
                     
 
-                    <div id='ap-div1-title' className="flex justify-center w-[100%] h-[auto] lg:h-[50px] border-black border-dashed border-[1px]">
+                    <div id='ap-div1-title' className="flex justify-center w-[100%] h-[auto]  border-black border-dashed border-[1px]">
                         <p className="font-awakenning antialiased text-[32px] md:text-[36px] md-lg:text-[42px] lg:text-[48px]  border-black border-dashed border-[1px]">About</p>
                     </div>
-                    <div id='ap-div1-summary' className="flex justify-center w-[100%]  h-[auto] lg:h-[50px] text-[16px] md:text-[24px] mt-[30px]  border-black border-dashed border-[1px]">
+                    <div id='ap-div1-summary' className="flex justify-center w-[100%]  h-[auto]  text-[16px] md:text-[24px] mt-[30px]  border-black border-dashed border-[1px]">
                         <p className="text-center">This section touches on my background, interests, and skills.</p>
                     </div>
                 </div>
@@ -22,12 +22,12 @@ const AboutPage = () =>{
 
             {/* Background, Interests and Skills Content */}
             <div id='ap-div1-content-container' className="mt-[90px] pr-[20px] pl-[20px] mb-[105px] lg:flex w-[100%]  border-black border-dashed border-[1px]">
-                <div id='background-interests' className="min-h-[365px] h-[auto] max-w-[555px] w-[100%] mb-[40px]  border-black border-dashed border-[1px]">
+                <div id='background-interests' className="lg:mr-[150px] min-h-[365px] h-[auto] max-w-[555px] w-[100%] mb-[40px]  border-black border-dashed border-[1px]">
                     <div id='bi-title' className="h-[auto] w-[100%] font-awakenning antialiased text-[24px] border-black border-dashed border-[1px]">
                         <p>My Background and Interests</p>
                     </div>
-                    <div id='bi-content' className="text-[14px] md:text-[16px]">
-                        <p className="text-left">I like coding, and I enjoy creating applications that people will use.  I began my web development journey after completing a MBA program.   I went to a bootcamp that spanned 14 months; it consisted of 11 months of full-stack development, and 3 months of Python development.  As a hobby, I like to study the Linux operating system, and other programming languages.   I hope to one day create or contribute to an open-source project.  I also have an interest in entrepreneurship.</p>
+                    <div id='bi-content' className="text-[14px] md:text-[16px] ">
+                        <p className="text-left leading-[30px]">I like coding, and I enjoy creating applications that people will use.  I began my web development journey after completing a MBA program.   I went to a bootcamp that spanned 14 months; it consisted of 11 months of full-stack development, and 3 months of Python development.  As a hobby, I like to study the Linux operating system, and other programming languages.   I hope to one day create or contribute to an open-source project.  I also have an interest in entrepreneurship.</p>
                     </div>
                 </div>
                 <div id='skills' className="min-h-[270px] h-[auto] w-[100%] border-black border-dashed border-[1px]">
