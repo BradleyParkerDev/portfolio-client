8835c17ff4e52a56343bd219cc0ed0d6be579d54
Bradley Parker <Bradleyscottparker@outlook.com>
2023-10-15 02:06:34 -0500

    added more to contact page

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -5,6 +5,13 @@ const ContactForm = () =>{
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
 
+    const messageObject = {
+        name:name,
+        emailAddress:emailAddress,
+        subject:subject,
+        message:message
+    }
+
     const handleNameChange = (e) =>{
         setName(e.target.value)
         console.log(name)
@@ -25,10 +32,13 @@ const ContactForm = () =>{
 
     }
 
+
+
     const contactSubmitButton = (messageObj) =>{
+        
         return(
-            <div id='submit-button' className="flex justify-center w-[110px] h-[40px] pt-[9px]  bg-portfolio-red rounded-[10px]">
-                <p className="font-awakenning text-white text-[20px] antialiased ">Submit</p>
+            <div id='submit-button' onClick={()=>{console.log(messageObj)}} className="flex justify-center w-[110px] h-[40px] pt-[6px]  bg-portfolio-red rounded-[10px]">
+                <p className="font-awakenning text-white text-[24px] antialiased ">Submit</p>
             </div>
         )
     }
@@ -37,20 +47,20 @@ const ContactForm = () =>{
             <div id='contact-form-container' className="w-[100%] max-w-[600px] h-[500px] rounded-[10px] bg-portfolio-grey border-solid border-[1px] border-black p-[10px]">
 
                 <div id='name-field' className="w-[100%] h-[40px] bg-white border-black border-solid border-[1px] rounded-[5px] mb-[25px]">
-                    <input type='text' value={name} className="w-[100%] h-[100%] text-[32px]" onChange={handleNameChange}/>
+                    <input type='text' placeholder="Name" value={name} className="w-[100%] h-[100%] text-[24px] pl-[5px] pr-[5px] outline-none rounded-[5px]" onChange={handleNameChange}/>
                 </div>
                 <div id='email-field' className="w-[100%] h-[40px] bg-white border-black border-solid border-[1px] rounded-[5px] mb-[25px]">
-                    <input type='text' value={emailAddress} className="w-[100%] h-[100%] text-[32px]" onChange={handleEmailChange}/>
+                    <input type='text' placeholder="Email Address" value={emailAddress} className="w-[100%] h-[100%] text-[24px] pl-[5px] pr-[5px] outline-none rounded-[5px]" onChange={handleEmailChange}/>
                 </div>
                 <div id='subject-field' className="w-[100%] h-[40px] bg-white border-black border-solid border-[1px] rounded-[5px] mb-[25px]">
-                    <input type='text' value={subject} className="w-[100%] h-[100%] text-[32px]" onChange={handleSubjectChange}/>
+                    <input type='text' placeholder="Subject" value={subject} className="w-[100%] h-[100%] text-[24px] pl-[5px] pr-[5px] outline-none rounded-[5px]" onChange={handleSubjectChange}/>
                 </div>
                 <div id='message-field' className="w-[100%] h-[280px] bg-white border-black border-solid border-[1px] rounded-[5px] mb-[25px]">
-                    <input type='text' value={message} className="w-[100%] h-[100%] text-[32px]" onChange={handleMessageChange}/>
+                    <textarea type='text' placeholder="Message" value={message} className="w-[100%] h-[100%] text-[24px] pl-[5px] pr-[5px] outline-none rounded-[5px]" onChange={handleMessageChange}/>
                 </div>
             </div>
             <div id='cs-button-div' className="flex justify-end pt-[30px] w-[100%] max-w-[600px] h-[auto]">
-                {contactSubmitButton()}
+                {contactSubmitButton(messageObject)}
             </div>
         </div>
 
